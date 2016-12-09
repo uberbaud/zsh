0a46576c1900485dd80d5c755cabc48657da3747
Tom Davis <tom@greyshirt.net>
2016-12-09 01:50:59 -0500

    Test before doing to keep error messages down

--- bin/rezsh.zsh
+++ bin/rezsh.zsh
@@ -1,21 +1,18 @@
 #!/usr/bin/env zsh
-# @(#)[rezsh.zsh 2016/10/27 04:49:41 tw@csongor.lan]
+# @(#)[:gfCr+Ry[rezsh.zsh 2016/10/27 04:49:41 tw@csongor.lan]<6wXwzgmasGj: 2016/12/09 06:50:49 tw@csongor.lan]
 # vim: filetype=zsh tabstop=4 textwidth=72 noexpandtab
 
 . $USR_ZSHLIB/common.zsh
 
-typeset -- save_wd=$PWD
 cd $ZDOTDIR
 
 typeset -- funclib='usrfuncs.zwc'
-typeset -- cfuncs=()
-typeset -- fbody=''
 zcompile -t $funclib | while read item; do
 	[[ $item =~ '^functions/' ]]|| continue
-	cfuncs+=( ${item:$MEND} )
+	item=${item:$MEND}
+	(($+functions[$item]))&& unfunction $item
 done
 
-(($#cfuncs))&& unfunction $cfuncs
 zcompile -Uz $funclib functions/*(.)
 autoload -Uz functions/*(.:t)
 
