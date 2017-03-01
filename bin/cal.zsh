4e00db7a8ab06fc8d59a6cdefa10c12dd8f7625f
Tom Davis <tom@greyshirt.net>
2017-03-01 02:12:41 -0500

    (Re)move pkg function, integrate calendar & cal
    Move pkg function to it's own autoload file.
    Output a calendar AND today+tomorrow's events in a unified format
    Add workaround to --msgify to handle for weirdness when a value has some
        numeric stuff that causes zsh to throw a bad math error

--- bin/cal.zsh
+++ bin/cal.zsh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
-# @(#)[cal.zsh 2016/10/25 07:07:16 tw@csongor.lan]
+# @(#)[:e0$Emzv9aDN4!Og3WY9T: 2017/03/01 07:08:35 tw@csongor.lan]
 # vim: filetype=zsh tabstop=4 textwidth=72 noexpandtab
 
 emulate -L zsh
 . $USR_ZSHLIB/common.zsh|| exit 86
 
+:needs /usr/bin/cal
 
 typeset -- today=$(date +%d)
 typeset -- search="${today/#0/ }\\>"
@@ -13,6 +14,55 @@ typeset -- stdopts=(
 	-m		# week starts on Monday (not Sunday)
 )
 
-/usr/bin/cal $stdopts $@ | /usr/bin/sed -E "s/($search)/$replace/"
+(($#))&& {
+	:needs /usr/bin/sed
+	/usr/bin/cal $stdopts $@ | /usr/bin/sed -E "s/($search)/$replace/g"
+	exit 0
+}
+
+:needs /usr/bin/calendar
+
+typeset -- nt=$'\n\t'	# multiline calendar events
+typeset -- day=''
+typeset -- ev='' e1='' pref=''
+typeset -a tuple=()
+typeset -a events=()
+typeset -- calblob=$(
+	/usr/bin/cal $stdopts | /usr/bin/sed -E "s/($search)/$replace/g"
+	)
+typeset -- evblob=$(/usr/bin/calendar)
+integer evsize=$((COLUMNS-23))
+
+evblob=${evblob//$nt/ }
+for ln in ${(f)evblob}; do
+	tuple=( ${(ps:\t:)ln} )
+	[[ $tuple[1] == $day ]]|| {
+		day=$tuple[1]
+		events+=( $'\e[1m'$day$'\e[22m' )
+	  }
+	ev=$tuple[2]
+	while (($#ev>evsize)); do
+		T=${pref}${${ev:0:$evsize}% *}
+		while [[ $T == *' ' ]] { T=${T:0:-1} } # remove trailing spaces
+		events+=( $T )
+		pref='    '
+		ev=${ev:$#T}
+		while [[ $ev == ' '* ]] { ev=${ev:1} } # remove leading spaces
+	done
+	events+=( ${pref}$ev )
+	pref=''
+done
+
+typeset -a cal=( ${(f)calblob} )
+
+# print calendar and events where it's one for one
+# the ANSI sequence \e[#G moves to the absolute column #
+while (($#cal&&$#events)); do
+	printf ' %s \e[23G %s\n' $cal[1] $events[1]
+	shift cal events
+done
+
+for c ($cal) printf ' %s\n' $c				# print any remaining calendar
+for e ($events) printf '\e[23G %s\n' $e		# print any remaining events
 
 # Copyright © 2016 by Tom Davis <tom@greyshirt.net>.
