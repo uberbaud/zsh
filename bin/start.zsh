5d99c37ad17431dc931a1ef1dbaf27e6d6fe3360
Tom Davis <tom@greyshirt.net>
2017-07-05 17:00:34 -0400

    Better handle amuse bits

--- bin/start.zsh
+++ bin/start.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-# @(#)[:!qoFKJRWTqV74SCjJrLd: 2017/07/04 08:42:47 tw@csongor.lan]
+# @(#)[:!qoFKJRWTqV74SCjJrLd: 2017/07/05 02:57:15 tw@csongor.lan]
 # vim: filetype=zsh tabstop=4 textwidth=72 noexpandtab nowrap
 
 emulate -L zsh
@@ -48,25 +48,50 @@ else
 	shift
 fi
 
-typeset -- appbin==$app # =$appbin is the executable path
-:needs $appbin xdotool
-#:needs mkdir nohup ln
+typeset -- strtcfg=${XDG_CONFIG_HOME}/start
+[[ -d $strtcfg ]]|| mkdir -p $strtcfg
+[[ -d $strtcfg ]]|| -die 'No %B$XDG_CONFIG_HOME/start%b directory.'
+
+typeset -- appcfg=${strtcfg}/${app}.ini
+typeset -- appbin=$(whence -p $app)
+[[ -n $appbin ]]|| {
+	[[ -f $appcfg ]]||
+		-die "No executable nor config file for %B${app:gs/%/%%}%b."
+  }
+
+typeset -- needCfgFile=false
+[[ -f $appcfg ]]		|| { # create config file or exit (user decides) {{{1
+	-warn "No configuration file \$XDG_CONFIG_HOME/start/$app.ini"
+	:yes-or-no Continue || exit 1
+	needCfgFile=true
+  } # }}}1
+
+REALHOME=$HOME
+HOME=$XDG_DATA_HOME/run/$app
+[[ -d $HOME ]] || mkdir -p $HOME
+cd $HOME
+on-error -die "Could not %Tcd%t to %B${HOME:gs/%/%%}%b."
+[[ -f ${HOME}/log ]]&& mv ${HOME}/log{,F}
+[[ -d ${HOME}/log ]]|| mkdir -p ${HOME}/log
+
+[[ -f .Xauthority ]]			|| ln -s ${REALHOME}/.Xauthority || -die 'Bad .Xauthority'
+[[ -d rxfer ]]					|| ln -s ${REALHOME}/rxfer
 
 XDG_DOCUMENTS_DIR+=/$app
-[[ -d $XDG_DATA_HOME ]]	|| mkdir -p $XDG_DATA_HOME
-typeset -- appcfg=${XDG_CONFIG_HOME}/start-app/${app}.ini
-[[ -d ${appcfg%/*} ]]	|| -warn 'No $XDG_CONFIG_HOME/start-app directory.'
-[[ -f $appcfg ]]		|| { # create config file or exit (user decides) {{{1
-	-warn "No configuration file \$XDG_CONFIG_HOME/start-app/$app.ini"
-	:yes-or-no Continue || exit 1
-	[[ -d ${appcfg%/*} ]]|| mkdir -p ${appcfg%/*}
-	-notify "Creating configuration file \$XDG_CONFIG_HOME/start-app/$app.ini"
+[[ -d $XDG_DOCUMENTS_DIR ]]	|| mkdir -p $XDG_DOCUMENTS_DIR
+[[ -d docs ]]|| ln -s $XDG_DOCUMENTS_DIR docs
+
+function mkCfgFile { # {{{1
+	-notify "Creating configuration file \$XDG_CONFIG_HOME/start/$app.ini"
 	cat >$appcfg <<----
 		; $(:stemma-header)
 		; vim: ft=dosini
-		; start-app ${app} configuration
+		; start ${app} configuration
 
 		desktop=xApp
+		appbin=${appbin}
+		; uncomment below to prevent multiple invocations
+		;oneinstance
 
 		; all shell special characters in environment assignment will be escaped.
 		; Enviroment variables are available in any following sections.
@@ -85,82 +110,78 @@ typeset -- appcfg=${XDG_CONFIG_HOME}/start-app/${app}.ini
 
 		; $(:copyright)
 	---
-  } # }}}1
+} #}}}1
 
-REALHOME=$HOME
-HOME=$XDG_DATA_HOME/run/$app
-[[ -d $HOME ]] || mkdir -p $HOME
-cd $HOME
-on-error -die "Could not %Tcd%t to %B${HOME:gs/%/%%}%b."
-[[ -f ${HOME}/log ]]&& mv ${HOME}/log{,F}
-[[ -d ${HOME}/log ]]|| mkdir -p ${HOME}/log
+typeset -- oneInstance=false
 
-[[ -f .Xauthority ]]			|| ln -s ${REALHOME}/.Xauthority || -die 'Bad .Xauthority'
-[[ -d rxfer ]]					|| ln -s ${REALHOME}/rxfer
+if $needCfgFile; then
+	mkCfgFile
+else
+	typeset -A dskNums=(
+		BROWSER		${dskBROWSER:-1}
+		XAPP		${dskXAPP:-6}
+		WIDGET		${dskWIDGET:-7}
+	  )
+	function badcfg { # {{{1
+		[[ $appcfg == ${XDG_CONFIG_HOME}/* ]]&& appcfg=\$XDG_CONFIG_HOME${appcfg#$XDG_CONFIG_HOME}
+		[[ $appcfg == ${HOME}/* ]]&& appcfg=\$HOME${appcfg#$HOME}
+		-die $@ "file: %S${appcfg:gs/%/%%}%s" "line $LNO: %T$ln%t"
+	} # }}}1
+	integer desktop=0
+	function optcmd-general { # {{{1
+		case $1 in
+			desktop)		desktop=${dskNums[${(U)2:-}]};			;;
+			oneinstance)	oneInstance=true;						;;
+			appbin)			appbin=$2;								;;
+			# UNKNOWN
+			*)	badcfg "Unknown configuration directive:" $1=$2
+		esac
+	} # }}}1
+	function optcmd-fsobject { # {{{1
+		typeset -- TRG=${HOME}/${1} t=-${3:0:1} fsType=$3
+		[ $t $TRG ]&& return 0
+		[[ -e $TRG ]]&& badcfg "%B${1:gs/%/%%}%b exists but is not a $fsType."
 
-typeset -- docspath=${XDG_DATA_HOME}/$app
-[[ -d $docspath ]]&&
-	{ [[ -d docs ]]|| ln -s $docspath docs }
+		[[ $TRG == \!* ]]&& {
+			[[ $t == '-d' ]]|| badcfg 'Only directories can be created.'
+			mkdir -p $HOME/${TRG:1}
+			return $?
+		  }
 
-function badcfg { # {{{1
-	[[ $appcfg == ${XDG_CONFIG_HOME}/* ]]&& appcfg=\$XDG_CONFIG_HOME${appcfg#$XDG_CONFIG_HOME}
-	[[ $appcfg == ${HOME}/* ]]&& appcfg=\$HOME${appcfg#$HOME}
-	-die $@ "file: %S${appcfg:gs/%/%%}%s" "line $LNO: %T$ln%t"
-} # }}}1
-integer desktop=0
-function optcmd-general { # {{{1
-	case $1 in
-		desktop)	eval "desktop=\$dsk${(U)2:-}";	;;
-		*)			badcfg "Unknown configuration directive:" $1=$2
-	esac
-} # }}}1
-function optcmd-fsobject { # {{{1
-	typeset -- TRG=${HOME}/${1} t=-${3:0:1} fsType=$3
-	[ $t $TRG ]&& return 0
-	[[ -e $TRG ]]&& badcfg "%B${1:gs/%/%%}%b exists but is not a $fsType."
+		typeset -- SRC="$(readlink -f ${REALHOME}/$2)"
+		[[ -n $SRC ]]|| return 1
 
-	[[ $TRG == \!* ]]&& {
-		[[ $t == '-d' ]]|| badcfg 'Only directories can be created.'
-		mkdir -p $HOME/${TRG:1}
-		return $?
-	  }
-
-	typeset -- SRC="$(readlink -f ${REALHOME}/$2)"
-	[[ -n $SRC ]]|| return 1
-
-	if [ $t $SRC ]; then
-		if [[ $SRC == ${REALHOME}/.* ]]; then
-			-warn "Moving %B${2:gs/%/%%}%b from %S\$HOME%s to %S\$XDG_DATA_HOME/run/${app:gs/%/%%}%s."
-			mv $SRC $TRG
-		else
-			ln -s $SRC $TRG ||
-				badcfg "Could not link %B${2:gs/%/%%}%b to %B${1:gs/%/%%}%b."
+		if [ $t $SRC ]; then
+			if [[ $SRC == ${REALHOME}/.* ]]; then
+				-warn "Moving %B${2:gs/%/%%}%b from %S\$HOME%s to %S\$XDG_DATA_HOME/run/${app:gs/%/%%}%s."
+				mv $SRC $TRG
+			else
+				ln -s $SRC $TRG ||
+					badcfg "Could not link %B${2:gs/%/%%}%b to %B${1:gs/%/%%}%b."
+			fi
+		elif [[ -e $SRC ]]; then
+			badcfg "%B${1:gs/%/%%}%b exists but is not a $fsType."
 		fi
-	elif [[ -e $SRC ]]; then
-		badcfg "%B${1:gs/%/%%}%b exists but is not a $fsType."
-	fi
-} # }}}1
-typeset -a prefixArgs=()
-function optcmd-prefix-args { prefixArgs+=( "${1}${2:+=$2}" ); }
-typeset -a suffixArgs=()
-function optcmd-suffix-args { suffixArgs+=( "${1}${2:+=$2}" ); }
-function optcmd-directory { optcmd-fsobject $1 ${2:-$1} directory; }
-function optcmd-file { optcmd-fsobject $1 ${2:-$1} file; }
-function optcmd-environment { # {{{1
-	(($#==2))||
-		badcfg 'Bad environment variable declaration.'
-	[[ $1 == ${(q)1} ]]||
-		badcfg "Bad environment variable name %B${2:gs/%/%%}%b."
-	eval "export $1=${(q)2}"||
-		badcfg "Could not export %B$1%b as %B'${2:gs/%/%%}%b'."
-	[[ $2 == ${(P)1} ]]||
-		badcfg "Could not use %B'${(q)2:gs/%/%%}%b' as a value."
-	[[ ${(Pt)1} == scalar-export ]]||
-		badcfg "Could not export %B'${1:gs/%/%%}%b'."
-} # }}}1
+	} # }}}1
+	typeset -a prefixArgs=()
+	function optcmd-prefix-args { prefixArgs+=( "${1}${2:+=$2}" ); }
+	typeset -a suffixArgs=()
+	function optcmd-suffix-args { suffixArgs+=( "${1}${2:+=$2}" ); }
+	function optcmd-directory { optcmd-fsobject $1 ${2:-$1} directory; }
+	function optcmd-file { optcmd-fsobject $1 ${2:-$1} file; }
+	function optcmd-environment { # {{{1
+		(($#==2))||
+			badcfg 'Bad environment variable declaration.'
+		[[ $1 == ${(q)1} ]]||
+			badcfg "Bad environment variable name %B${2:gs/%/%%}%b."
+		eval "export $1=${(q)2}"||
+			badcfg "Could not export %B$1%b as %B'${2:gs/%/%%}%b'."
+		[[ $2 == ${(P)1} ]]||
+			badcfg "Could not use %B'${(q)2:gs/%/%%}%b' as a value."
+		[[ ${(Pt)1} == scalar-export ]]||
+			badcfg "Could not export %B'${1:gs/%/%%}%b'."
+	} # }}}1
 
-
-[[ -f $appcfg ]]&& {
 	typeset -- docmd='general'
 	typeset -a validcmds=(
 		general directory file environment prefix-args suffix-args
@@ -169,9 +190,9 @@ function optcmd-environment { # {{{1
 	for ln in ${(f)"$(<$appcfg)"}; do
 		((LNO++))
 		# COMMENTS & BLANK LINES
-		[[ $ln == \;* ]]&& continue
-		[[ $ln == \#* ]]&& continue
-		[[ $ln =~ '^[[:blank:]]$' ]]&& continue
+		[[ $ln == \;* ]]&&				continue
+		[[ $ln == \#* ]]&&				continue
+		[[ $ln =~ '^[[:blank:]]*$' ]]&&	continue
 		# HEADING
 		[[ $ln =~ '\[(.*)\]' ]]&& {
 			docmd=$match[1]
@@ -186,11 +207,26 @@ function optcmd-environment { # {{{1
 		  }
 		badcfg "Unknown configuration directive."
 	done
+	argv=( $prefixArgs $@ $suffixArgs )
+fi
+
+# switch to the requested desktop
+:needs $appbin
+((desktop))&& {
+	:needs xdotool
+	xdotool set_desktop $desktop
   }
 
-((desktop))&&
-	xdotool set_desktop $desktop
-nohup $appbin $prefixArgs $@ $suffixArgs > log/$$ 2>&1 &!
+# don't start a new process if we're not supposed to
+$oneInstance && {
+	pgrep -q "^${appbin##*/}$" && {
+		-notify 'Already running'
+		exit 0
+	  }
+}
+
+# and finally, start it up!
+nohup $appbin $@ > log/$$ 2>&1 &!
 mv log/$$ log/$!
 
 # Copyright © 2016 by Tom Davis <tom@greyshirt.net>.
